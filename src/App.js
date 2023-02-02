41abb0450c29e0cae7d081ce7163c4ac45a671e5
daryusoctavyan <dariuschifor@gmail.com>
2023-02-02 13:01:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-Header">
+        <p className='Header-Text'>Weather Station Data</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
