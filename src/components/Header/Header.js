6229a99da1c0d6355173424b55b3307215f8ee9c
daryusoctavyan <dariuschifor@gmail.com>
2023-02-08 19:15:41 +0200

    restructured to wwwroot

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./Header.css";
-import App from "../../App";
+import App from "../../wwwroot/App/App";
 
 function Header() {
   return (
--- src/wwwroot/App/App.js
+++ src/wwwroot/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header/Header";
+import Header from "../../components/Header/Header";
 
 function App() {
   return (
