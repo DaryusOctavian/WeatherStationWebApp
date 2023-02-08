1fc43316e5352287be358dd92d3a59c2d7e399f9
daryusoctavyan <dariuschifor@gmail.com>
2023-02-08 10:02:25 +0200

    added very ugly header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-Header">
         <p className='Header-Text'>Weather Station Data</p>
       </header>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='Header'>
+            <h1>proba</h1>
+        </div>
+    )
+}
+
+export default Header
